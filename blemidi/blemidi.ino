60aaca71cdc899149d7370a341f2d1c290b524d5
Adam Kalayjian <roboboy@kalayjian.org>
2023-04-25 19:50:39 -0400

    changes to firmware

--- blemidi/blemidi.ino
+++ blemidi/blemidi.ino
@@ -20,42 +20,64 @@
  */
 
 
+#include <Arduino.h>
 #include <bluefruit.h>
 #include <MIDI.h>
 #include <LSM6DS3.h>
+#include <MadgwickAHRS.h>
+#include <base64.hpp>
+
+#define CC_YAW 70
+#define CC_PITCH 71
+#define CC_ROLL 72
+#define MIDI_DICE 59
 
 BLEDis bledis;
 BLEMidi blemidi;
-LSM6DS3 myIMU(I2C_MODE, 0x6A);
+LSM6DS3 imu(I2C_MODE, 0x6A);
+
+// initialize a Madgwick filter:
+Madgwick filter;
+// sensor's sample rate is fixed at:
+const int sensorRate = 104;
+const float gyroThresh = 0.15;
+const float accelThresh = 0.013;
+int channel = 1; // this can change if interference detected
+unsigned long microsPerReading, microsPrevious;
 
 // Create a new instance of the Arduino MIDI Library,
 // and attach BluefruitLE MIDI as the transport.
 MIDI_CREATE_BLE_INSTANCE(blemidi);
 
-// Variable that holds the current position in the sequence.
-unsigned int position = 0;
-
-// Store example melody as an array of note values
-byte note_sequence[] = {
-  74,78,81,86,90,93,98,102,57,61,66,69,73,78,81,85,88,92,97,100,97,92,88,85,81,78,
-  74,69,66,62,57,62,66,69,74,78,81,86,90,93,97,102,97,93,90,85,81,78,73,68,64,61,
-  56,61,64,68,74,78,81,86,90,93,98,102
-};
-
-void setup()
-{
+void setup() {
   Serial.begin(115200);
-  while ( !Serial ) delay(10);   // for nrf52840 with native usb
-  myIMU.begin();
+
+  unsigned long start_time = millis();
+  while (!Serial && (millis() - start_time < 5000)) {
+    delay(10);
+  }
+
+  // Generate a unique Base64-encoded string based on device's UID
+  uint8_t uid[16];
+  memcpy(uid, (const void *)NRF_FICR->DEVICEADDR, sizeof(uid));
+  unsigned char unique_string[11];
+  encode_base64(uid, sizeof(uid), unique_string);
+  unique_string[10] = '\0';
+  // Print the unique string
+  Serial.println((char *)unique_string);
+
+
+
+
   Serial.println("Adafruit Bluefruit52 MIDI over Bluetooth LE Example");
 
-  // Config the peripheral connection with maximum bandwidth 
+  // Config the peripheral connection with maximum bandwidth
   // more SRAM required by SoftDevice
   // Note: All config***() function must be called before begin()
-  Bluefruit.configPrphBandwidth(BANDWIDTH_MAX);  
+  Bluefruit.configPrphBandwidth(BANDWIDTH_MAX);
 
   Bluefruit.begin();
-  Bluefruit.setTxPower(4);    // Check bluefruit.h for supported values
+  Bluefruit.setTxPower(4);  // Check bluefruit.h for supported values
 
   // Setup the on board blue LED to be enabled on CONNECT
   Bluefruit.autoConnLed(true);
@@ -75,16 +97,51 @@ void setup()
 
   // Do the same for MIDI Note Off messages.
   MIDI.setHandleNoteOff(handleNoteOff);
-  
+
+  MIDI.setHandleControlChange(handleControlChange);
+
+  MIDI.setHandleSystemExclusive()
+
   // Set up and start advertising
-  startAdv();
+  startAdv((char *)unique_string);
 
   // Start MIDI read loop
   Scheduler.startLoop(midiRead);
+
+  imu.settings.gyroEnabled = 1;              //Can be 0 or 1
+  imu.settings.gyroRange = 2000;             //Max deg/s.  Can be: 125, 245, 500, 1000, 2000
+  imu.settings.gyroSampleRate = sensorRate;  //Hz.  Can be: 13, 26, 52, 104, 208, 416, 833, 1666
+  imu.settings.gyroBandWidth = 200;          //Hz.  Can be: 50, 100, 200, 400;
+  // imu.settings.gyroFifoEnabled = 1;  //Set to include gyro in FIFO
+  // imu.settings.gyroFifoDecimation = 1;  //set 1 for on /1
+
+  imu.settings.accelEnabled = 1;
+  imu.settings.accelRange = 2;                //Max G force readable.  Can be: 2, 4, 8, 16
+  imu.settings.accelSampleRate = sensorRate;  //Hz.  Can be: 13, 26, 52, 104, 208, 416, 833, 1666, 3332, 6664, 13330
+  imu.settings.accelBandWidth = 200;          //Hz.  Can be: 50, 100, 200, 400;
+  // imu.settings.accelFifoEnabled = 1;  //Set to include accelerometer in the FIFO
+  // imu.settings.accelFifoDecimation = 1;  //set 1 for on /1
+  imu.settings.tempEnabled = 1;
+
+  imu.begin();
+
+  filter.begin(sensorRate);
+
+  // initialize variables to pace updates to correct rate
+  microsPerReading = 1000000 / sensorRate;
+  microsPrevious = micros();
+
+  discoverCubes();
+
 }
 
-void startAdv(void)
-{
+void startAdv(char *unique_string) {
+
+  char name[30];
+  strcpy(name, "Cube ");
+  strcat(name, unique_string);
+
+  Bluefruit.setName(name);
   // Set General Discoverable Mode flag
   Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
 
@@ -108,118 +165,223 @@ void startAdv(void)
    * https://developer.apple.com/library/content/qa/qa1931/_index.html   
    */
   Bluefruit.Advertising.restartOnDisconnect(true);
-  Bluefruit.Advertising.setInterval(32, 244);    // in unit of 0.625 ms
-  Bluefruit.Advertising.setFastTimeout(30);      // number of seconds in fast mode
-  Bluefruit.Advertising.start(0);                // 0 = Don't stop advertising after n seconds  
+  Bluefruit.Advertising.setInterval(32, 244);  // in unit of 0.625 ms
+  Bluefruit.Advertising.setFastTimeout(30);    // number of seconds in fast mode
+  Bluefruit.Advertising.start(0);              // 0 = Don't stop advertising after n seconds
 }
 
-void handleNoteOn(byte channel, byte pitch, byte velocity)
-{
+void handleNoteOn(byte channel, byte pitch, byte velocity) {
   // Log when a note is pressed.
   Serial.printf("Note on: channel = %d, pitch = %d, velocity - %d", channel, pitch, velocity);
   Serial.println();
 }
 
-void handleNoteOff(byte channel, byte pitch, byte velocity)
-{
+void handleNoteOff(byte channel, byte pitch, byte velocity) {
   // Log when a note is released.
   Serial.printf("Note off: channel = %d, pitch = %d, velocity - %d", channel, pitch, velocity);
   Serial.println();
 }
 
-float ax = 0;
-float ay = 0;
-float az = 0;
-float gx = 0;
-float gy = 0;
-float gz = 0;
+void handleControlChange(byte channel, byte control, byte value) {
 
-int prevValue1 = 0;
-int prevValue2 = 0;
-void getIMU() {
-  ax = myIMU.readFloatAccelX();
-  ay = myIMU.readFloatAccelY();
-  az = myIMU.readFloatAccelZ();
+
 }
 
-void loop()
-{
+
+// Set up filter coefficients (adjust as needed)
+float alpha = 1;
+float dt = 0.005;
+
+float heading = 0;
+float pitch = 0;
+float roll = 0;
+
+int prevOutputH, prevOutputP, prevOutputR;
+int outputH = 64;
+int outputP = 64;
+int outputR = 64;
+
+// Variables to store the previous readings.
+float prev_ax = 0, prev_ay = 0, prev_az = 0;
+float prev_gx = 0, prev_gy = 0, prev_gz = 0;
+
+int stillCounter = 0;
+int movingCounter = 0;
+bool isMoving;
+int lastDice = 0;
+void loop() {
   // Don't continue if we aren't connected.
-  if (! Bluefruit.connected()) {
+  if (!Bluefruit.connected()) {
     return;
   }
 
   // Don't continue if the connected device isn't ready to receive messages.
-  if (! blemidi.notifyEnabled()) {
+  if (!blemidi.notifyEnabled()) {
     return;
   }
+  unsigned long microsNow;
 
-  // Setup variables for the current and previous
-  // positions in the note sequence.
-  // unsigned int current = position;
-  // If we currently are at position 0, set the
-  // previous position to the last note in the sequence.
-  // unsigned int previous = (current == 0) ? (sizeof(note_sequence)-1) : current - 1;
+  float ax, ay, az;  // Acceleration readings
+  float gx, gy, gz;  // Gyroscope readings
 
-  // Send Note On for current position at full velocity (127) on channel 1.
-  // MIDI.sendNoteOn(note_sequence[current], 127, 1);
 
-  // Send Note Off for previous note.
-  // MIDI.sendNoteOff(note_sequence[previous], 0, 1);
 
-  // Increment position
-  // position++;
 
-  // If we are at the end of the sequence, start over.
-  // if (position >= sizeof(note_sequence)) {
-  //   position = 0;
-  // }
-  getIMU();
+  // check if it's time to read data and update the filter
+  microsNow = micros();
 
-  double angle1 = atan2((double)ay, (double)ax);
-  double angle2 = atan2((double)az, (double)ax);
+  // check if the IMU is ready to read:
+  if (microsNow - microsPrevious >= microsPerReading) {
+    // read accelerometer & gyrometer:
+    ax = imu.readFloatAccelX();
+    ay = imu.readFloatAccelY();
+    az = imu.readFloatAccelZ();
+    gx = imu.readFloatGyroX();
+    gy = imu.readFloatGyroY();
+    gz = imu.readFloatGyroZ();
+
+    // update the filter, which computes orientation:
+    filter.updateIMU(gx, gy, gz, ax, ay, az);
+
+    // print the heading, pitch and roll
+    roll = filter.getRoll();
+    pitch = filter.getPitch();
+    heading = filter.getYaw();
+
+    // increment previous time, so we keep proper pace
+    microsPrevious = microsPrevious + microsPerReading;
+
+    // Calculate the change in acceleration and angular velocity.
+    float delta_ax = absolute_difference(ax, prev_ax);
+    float delta_ay = absolute_difference(ay, prev_ay);
+    float delta_az = absolute_difference(az, prev_az);
+    float delta_gx = absolute_difference(gx, prev_gx);
+    float delta_gy = absolute_difference(gy, prev_gy);
+    float delta_gz = absolute_difference(gz, prev_gz);
+
+    // Update the previous values.
+    prev_ax = ax; prev_ay = ay; prev_az = az;
+    prev_gx = gx; prev_gy = gy; prev_gz = gz;
+
+     // Calculate the magnitudes of the change in acceleration and angular velocity.
+    float delta_accel_magnitude = magnitude(delta_ax, delta_ay, delta_az);
+    float delta_gyro_magnitude = magnitude(delta_gx, delta_gy, delta_gz);
+
+    // Print the magnitudes to the serial monitor.
+    bool nowMoving = false;
+    if (delta_accel_magnitude < accelThresh && delta_gyro_magnitude < gyroThresh) {
+      nowMoving = false;
+    } else {
+      nowMoving = true;
+    }
+
+    if (nowMoving) {
+      stillCounter = 0;
+      movingCounter++;      
+    } else {
+      movingCounter = 0;
+      stillCounter++;
+    }
+
+    if (stillCounter > 10 && isMoving) {
+      isMoving = false;
+      movingCounter = 0;
+      // Serial.println("STILL");
+      int pitch = MIDI_DICE + calculateDice(ax, ay, az);
+      MIDI.sendNoteOn(pitch, 60, channel);
+      lastDice = pitch;
+      
+    } else if (movingCounter > 10 && !isMoving) {
+      isMoving = true;
+      stillCounter = 0;
+      Serial.println("MOVING");
+    }
+    
+     
+    outputH = (int)((heading / 180.0 + 1) / 2 * 127);
+    outputR = (int)((roll / 180.0 + 1) / 2 * 127);
+    outputP = (int)((pitch / 180.0 + 1) / 2 * 127);
+    if (isMoving) {
+      if (lastDice != 0) {
+        MIDI.sendNoteOff(lastDice, 0, channel);
+      }
+      if (outputH != prevOutputH) {
+        prevOutputH = outputH;
+        MIDI.sendControlChange(CC_YAW, (int)outputH, channel);
+      }
+      if (outputR != prevOutputR) {
+        prevOutputR = outputR;
+        MIDI.sendControlChange(CC_ROLL, (int)outputR, channel);
+      }
+      if (outputP != prevOutputP) {
+        prevOutputP = outputP;
+        MIDI.sendControlChange(CC_PITCH, (int)outputP, channel);
+      }
+    }
+  }
   // double angle = 5.0;
-  Serial.print(angle1);
-  Serial.print(" ");
-  Serial.println(angle2);
-  int value1 = (int)((angle1/PI+1.0)/2*127);
-  int value2 = (int)((angle2/PI+1.0)/2*127);
-  Serial.print(angle1);
-  Serial.print(" ");
-  Serial.print(angle1);
-  Serial.print(" ");
-  Serial.print(value1);
-  Serial.print(" ");
-  Serial.println(value2);
-  if (value1 != prevValue1) {
-    MIDI.sendControlChange(0,(int)value1, 1);  
-    prevValue1 = value1;
-  }
-  if (value2 != prevValue2) {
-    MIDI.sendControlChange(0,(int)value2, 1);  
-    prevValue2 = value2;
-  }
+  // int value1 = (int)((angle1/PI+1.0)/2*127);
+  // int value2 = (int)((angle2/PI+1.0)/2*127);
+  
+  // if (value1 != prevValue1) {
+  //   MIDI.sendControlChange(0,(int)value1, 1);
+  //   prevValue1 = value1;
+  // }
+  // if (value2 != prevValue2) {
+  //   MIDI.sendControlChange(0,(int)value2, 1);
+  //   prevValue2 = value2;
+  // }
 
-  delay(100);
 
+
+  midiRead();
 }
 
-
-
-
-void midiRead()
-{
+void midiRead() {
   // Don't continue if we aren't connected.
-  if (! Bluefruit.connected()) {
+  if (!Bluefruit.connected()) {
     return;
   }
 
   // Don't continue if the connected device isn't ready to receive messages.
-  if (! blemidi.notifyEnabled()) {
+  if (!blemidi.notifyEnabled()) {
     return;
   }
 
   // read any new MIDI messages
   MIDI.read();
 }
+
+// Function to calculate the absolute difference between two values.
+float absolute_difference(float a, float b) {
+  return abs(a - b);
+}
+
+int calculateDice(float x, float y, float z) {
+  const float threshold = 0.8;
+
+  if (x > threshold) {
+    return 1;
+  } else if (x < -threshold) {
+    return 6;
+  } else if (y > threshold) {
+    return 2;
+  } else if (y < -threshold) {
+    return 5;
+  } else if (z > threshold) {
+    return 3;
+  } else if (z < -threshold) {
+    return 4;
+  } else {
+    // In case the accelerometer is not in a stable position
+    return -1;
+  }
+}
+
+
+// Function to calculate the magnitude of a vector.
+float magnitude(float x, float y, float z) {
+  return sqrt(x * x + y * y + z * z);
+}
+
 
